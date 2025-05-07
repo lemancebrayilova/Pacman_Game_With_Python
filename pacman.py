973e67b544a0f53c2bcacce91cbfebef612f7588
lemancebrayilova <cbrayilovalman33@gmail.com>
2025-05-07 22:41:40 +0300

    Initialize project structure

---
+++ pacman.py
@@ -0,0 +1,23 @@
+# Build Pac-Man from Scracth in Python with PyGame!!
+import pygame
+
+pygame.init()
+
+WIDTH = 700
+HEIGHT = 750
+screen = pygame.display.set_mode([WIDTH, HEIGHT])
+timer = pygame.time.Clock()
+fps = 60
+font = pygame.font.Font('freesansbold.ttf', 20)
+
+run = True
+while run:
+    timer.tick(fps)
+    screen.fill('black')
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+    pygame.display.flip()
+pygame.quit()

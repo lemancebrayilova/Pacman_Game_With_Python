055c532ad20a472259e113138b8c7ade6cdb707b
lemancebrayilova <cbrayilovalman33@gmail.com>
2025-05-29 00:13:23 +0300

    Fixed minor bugs

--- pacman.py
+++ pacman.py
@@ -678,13 +678,13 @@ def draw_misc():
 
 def check_collisions(scor, power, power_count, eaten_ghosts):
     num1 = (HEIGHT - 50) // 32
-    num2 = WIDTH//30
+    num2 = WIDTH // 30
     if 0 < player_x < 870:
-        if level[center_y//num1][center_x // num2] == 1:
-            level[center_y//num1][center_x // num2] = 0
+        if level[center_y // num1][center_x // num2] == 1:
+            level[center_y // num1][center_x // num2] = 0
             scor += 10
-        if level[center_y//num1][center_x // num2] == 2:
-            level[center_y//num1][center_x // num2] = 0
+        if level[center_y // num1][center_x // num2] == 2:
+            level[center_y // num1][center_x // num2] = 0
             scor += 50
             power = True
             power_count = 0
@@ -727,11 +727,11 @@ def draw_board():
 
 
 def draw_player():
-    #0-RIGHT, 1-LEFT, 2-UP, 3-DOWN
+    # 0-RIGHT, 1-LEFT, 2-UP, 3-DOWN
     if direction == 0:
         screen.blit(player_images[counter // 5], (player_x, player_y))
     elif direction == 1:
-        screen.blit(pygame.transform.flip(player_images[counter // 5],True, False), (player_x, player_y))
+        screen.blit(pygame.transform.flip(player_images[counter // 5], True, False), (player_x, player_y))
     elif direction == 2:
         screen.blit(pygame.transform.rotate(player_images[counter // 5], 90), (player_x, player_y))
     elif direction == 3:
@@ -740,46 +740,44 @@ def draw_player():
 
 def check_position(centerx, centery):
     turns = [False, False, False, False]
-    num1 = (HEIGHT-50)//32
-    num2 = (WIDTH//30)
+    num1 = (HEIGHT - 50) // 32
+    num2 = (WIDTH // 30)
     num3 = 15
-    # check collisions based on center x and center y of player */- fudge number
+    # check collisions based on center x and center y of player +/- fudge number
     if centerx // 30 < 29:
         if direction == 0:
-            if level[centery//num1][(centerx - num3)// num2] < 3:
+            if level[centery // num1][(centerx - num3) // num2] < 3:
                 turns[1] = True
         if direction == 1:
-            if level[centery//num1][(centerx + num3)// num2] < 3:
+            if level[centery // num1][(centerx + num3) // num2] < 3:
                 turns[0] = True
         if direction == 2:
-            if level[(centery+num3)//num1][centerx// num2] < 3:
+            if level[(centery + num3) // num1][centerx // num2] < 3:
                 turns[3] = True
         if direction == 3:
-            if level[(centery-num3)//num1][centerx// num2] < 3:
+            if level[(centery - num3) // num1][centerx // num2] < 3:
                 turns[2] = True
-
         if direction == 2 or direction == 3:
             if 12 <= centerx % num2 <= 18:
-                if level[(centery + num3)//num1][centerx // num2] < 3:
+                if level[(centery + num3) // num1][centerx // num2] < 3:
                     turns[3] = True
                 if level[(centery - num3)//num1][centerx // num2] < 3:
                     turns[2] = True
             if 12 <= centery % num1 <= 18:
-                if level[centery//num1][(centerx - num2)// num2] < 3:
+                if level[centery // num1][(centerx - num2) // num2] < 3:
                     turns[1] = True
-                if level[centery//num1][(centerx + num2)// num2] < 3:
+                if level[centery // num1][(centerx + num2) // num2] < 3:
                     turns[0] = True
-
         if direction == 0 or direction == 1:
             if 12 <= centerx % num2 <= 18:
-                if level[(centery + num1)//num1][centerx // num2] < 3:
+                if level[(centery + num1) // num1][centerx // num2] < 3:
                     turns[3] = True
-                if level[(centery - num1)//num1][centerx // num2] < 3:
+                if level[(centery - num1) //num1][centerx // num2] < 3:
                     turns[2] = True
             if 12 <= centery % num1 <= 18:
-                if level[centery//num1][(centerx - num3)// num2] < 3:
+                if level[centery // num1][(centerx - num3) // num2] < 3:
                     turns[1] = True
-                if level[centery//num1][(centerx + num3)// num2] < 3:
+                if level[centery // num1][(centerx + num3) // num2] < 3:
                     turns[0] = True
 
     else:
